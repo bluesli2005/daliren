fab089925ff4845d01e5a7122643c74cfd106b72
bunhyoLI <p.li.bunhyo@firstgroup.jp>
2021-02-22 18:15:33 +0900

    add dailiren

---
+++ store/modules/home.js
@@ -0,0 +1,12 @@
+const home = {
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+}
+
+export default home
---
+++ store/modules/mine.js
@@ -0,0 +1,12 @@
+const mine = {
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+}
+
+export default mine
